6f4c7192af6d770c45943633af6e4f4334732b2f
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-11-12 18:46:08 +0300

    Added Card component

---
+++ src/components/Card/Card.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+  background-color: #fff;
+  border-radius: 16px;
+  box-shadow: 0 4px 33px 0 #0000000a;
+`;
