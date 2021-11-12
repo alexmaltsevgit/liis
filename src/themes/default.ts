4ebe9d0e3eff20668d3f0356dca87dfbeb8e9065
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-11-12 19:09:46 +0300

    Added theme

---
+++ src/themes/default.ts
@@ -0,0 +1,14 @@
+import { AppTheme } from "./types";
+
+const defaultTheme: AppTheme = {
+  colors: {
+    red: "#E55858",
+    yellow: "#CDBC1E",
+    text: {
+      primary: "#424242",
+      secondary: "#878787",
+    },
+  },
+};
+
+export default defaultTheme;
---
+++ src/themes/types.ts
@@ -0,0 +1,10 @@
+export type AppTheme = {
+  colors: {
+    red?: string;
+    yellow?: string;
+    text: {
+      primary: string;
+      secondary: string;
+    };
+  };
+};
