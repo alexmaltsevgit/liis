fac00d0847beaee2d32e70ddf7e758cea0e83706
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-11-12 19:12:40 +0300

    Updated App component

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -74,31 +74,4 @@ export const GlobalStyle = createGlobalStyle`
     line-height: 0;
     font-weight: 0;
   };
-
-  input {
-    display: block;
-    width: 100%;
-    border-radius: 4px;
-
-    &:not([type="submit"]) {
-      padding: 10px 20px;
-      border: 1px #c9cacc solid;
-
-      &:focus {
-        outline: none;
-      }
-    }
-
-    &[type="submit"] {
-      background: linear-gradient(104.34deg, #41522e -15.34%, #be8022 145.95%);
-      border: 1px;
-      color: #fff;
-      font-weight: 500;
-
-      &:hover,
-      &:active {
-        cursor: pointer;
-      }
-    }
-  }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import React from "react";
-import "./App.scss";
-import { Route, Switch } from "react-router-dom";
-import Home from "./pages/Home.page";
-import SignIn from "./pages/SignIn.page";
+import React, { Fragment } from "react";
+import "./App.styles";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home.page";
+import SignIn from "./pages/SignIn/SignIn.page";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./App.styles";
+import defaultTheme from "./themes/default";
+import { RootState } from "./store/store";
+import { useSelector } from "react-redux";
+import Header from "./components/Header/Header.component";
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path={"/"}>
-        <Home />
-      </Route>
+  const isAuthorized = useSelector(
+    (state: RootState) => state.user.isAuthorized
+  );
 
-      <Route exact path={"/sign-in"}>
-        <SignIn />
-      </Route>
-    </Switch>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Switch>
+        {/* Routes without header */}
+        <Route exact path={"/sign-in"}>
+          {isAuthorized ? <Redirect to={"/"} /> : <SignIn />}
+        </Route>
+
+        {/* Routes with header*/}
+        <Fragment>
+          <Header />
+          <Route exact path={"/"}>
+            {isAuthorized ? <Home /> : <Redirect to={"/sign-in"} />}
+          </Route>
+        </Fragment>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
