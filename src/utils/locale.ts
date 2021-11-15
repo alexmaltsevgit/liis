23f3bd19e5c0666b60ab3755a786bbad7f9d01cc
Alexander Maltsev <orangealexmaltsev@gmail.com>
2021-11-15 03:11:49 +0300

    Added locale utils

---
+++ src/utils/locale.ts
@@ -0,0 +1,9 @@
+// https://gist.github.com/realmyst/1262561
+export const declOfNum = (number: number, titles: Array<string>) => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ];
+};
